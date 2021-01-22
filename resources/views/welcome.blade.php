9efd000a30d4710b639001f757e1e79d0666d4ea
Mpaliota Maria <marbaliota@gmail.com>
2021-01-22 20:17:15 +0200

    commit project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+   <head>
+		<!-- bootstrap -->
+   		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+   		<title>Recipes & Products</title>
+		<meta name="authors" value="G.Zazanhs & M.Baliota">
+		<style>
+        	body {
+        		background-image: url("https://i.ibb.co/mbwjkhL/141315180-726405331348614-7575132151216588057-n.jpg");
+            	background-repeat: no-repeat;
+            	background-attachment: fixed;
+            	background-size: cover;           
+        	}
+    	</style>
+	</head>
+
+
+	<div class="nav topnav">
+		<div>
+  			<a style="margin: 5px; margin-left: 40%;" href="/" class="btn btn-info active">Home</a>
+			<a style="margin: 5px;" href="/products" class="btn btn-info">Products</a>
+			<a style="margin: 5px;" href="/recipes" class="btn btn-info">Recipes</a>
+  		</div>   
+	</div>
+
+	<body>
+	
+	</body>
+
+
+</html>
+
